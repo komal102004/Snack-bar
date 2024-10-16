4b783f1ac5b92a32846c482c97a818311958aef3
komal <komalagrahari2004@gmail.com>
2024-10-17 03:34:42 +0530

    new update

---
+++ app.js
@@ -0,0 +1,21 @@
+let toastbox=document.getElementById('toastBox');
+let sucessMsg ='<i class="fa-solid fa-circle-check"></i>Successfully sumbitted';
+let errorMsg ='<i class="fa-solid fa-circle-xmark"></i>Please fix the error!';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i>Invalid input,check again';
+function showToast(msg){
+    let toast=document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML=msg;
+    toastbox.appendChild(toast);
+
+if(msg.includes('error'))
+{
+    toast.classList.add('error');  
+}
+if(msg.includes('Invalid')){
+    toast.classList.add('invalid');
+}
+setTimeout(()=>{
+    toast.remove();
+},6000);
+}
